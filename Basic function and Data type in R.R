8fd1c887de60770824c5782e88aea68d0423957f
Diannita Eka Putri <53862978+DiannitaOlipmimi@users.noreply.github.com>
2021-06-07 09:50:09 +0700

    basic R

---
+++ Basic function and Data type in R.R
@@ -0,0 +1,52 @@
+#assignment panah dan =
+a <- 10
+b = 5.7
+
+#data type integer (bil. bulat)
+5+10
+10+7.5
+
+c= 5+6.2-0.1
+c
+4^2
+7/2
+
+#menyimpan hasil ke variabel lain
+b1=a+b
+b2=a*b-5
+
+#data type string (huruf)
+"nama"
+'saya'
+#error
+'10' + 9 
+
+#menggabungkan data type
+paste("saya","adalah",1)
+paste("saya","adalah")
+paste0("saya","adalah")
+
+#data type boolean/logical
+#membangingkan ==
+1+5 == 6
+
+1+5<= 6
+
+benarkah = 14+7 == 7
+benarkah
+
+benarkah = 14+7 == 21
+benarkah
+
+#special value, NA (not available) dan null (himp kosong)
+#inf (tak hingga) dan NaN (bukan bilangan)
+10/0
+-10/0
+10 -Inf
+0/0
+a = NA
+b = NaN
+c = 100/0
+a
+b
+c
